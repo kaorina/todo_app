c08406fb0e12fe36a5af48bc4018214549524d4f
Kaori Asakura <zonorina@gmail.com>
2021-09-27 18:04:02 +0900

    Pokemonコントローラー作成、routes追加

---
+++ app/controllers/pokemons_controller.rb
@@ -0,0 +1,7 @@
+class PokemonsController < ApplicationController
+
+  def new
+    raw_response = Faraday.get "https://pokeapi.co/api/v2/pokemon/pikachu"
+    @response = JSON.parse(raw_response.body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
+
   resources :users
+
   resources :tasks
+
+  resources :pokemons
 
   namespace :api do
     namespace :v1 do
